d7f4c25573640d49cc4865d412a9093096fec57a
arturparkhisenko <ikeagold@gmail.com>
2018-10-04 00:00:51 -0400

    changed logic about injected scripts prefix

--- server/loader.js
+++ server/loader.js
@@ -112,7 +112,7 @@ export default (req, res) => {
 
           // Let's format those assets into pretty <script> tags
           const extraChunks = extractAssets(manifest, modules).map(
-            c => `<script type="text/javascript" src="/${c}"></script>`
+            c => `<script type="text/javascript" src="/${c.replace(/^\//, '')}"></script>`
           );
 
           // We need to tell Helmet to compute the right meta tags, title, and such
