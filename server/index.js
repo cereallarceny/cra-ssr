4c1c2d20e82f914bed4475dc39d45ddff07ac10b
Artur Parkhisenko <arturparkhisenko@users.noreply.github.com>
2018-10-04 08:15:48 -0400

    added note about polyfill, replaced double quotes

--- server/index.js
+++ server/index.js
@@ -26,7 +26,8 @@ register(ignoreStyles.DEFAULT_EXTENSIONS, (mod, filename) => {
 
 // Set up babel to do its thing... env for the latest toys, react-app for CRA
 // Notice three plugins: the first two allow us to use import rather than require, the third is for code splitting
-require("@babel/polyfill");
+// Polyfill is required for Babel 7, polyfill includes a custom regenerator runtime and core-js
+require('@babel/polyfill');
 require('@babel/register')({
   ignore: [/\/(build|node_modules)\//],
   presets: ['@babel/preset-env', '@babel/preset-react'],
