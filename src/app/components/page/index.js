be954892a632efd9b200bfe17507ccc5c9da4240
cereallarceny <me@patrickcason.com>
2019-07-04 11:41:05 +0100

    Updating github URL for moonshot

--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router';
-import Helmet from 'react-helmet';
-import logo from '../../assets/logo.jpg';
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import Helmet from "react-helmet";
+import logo from "../../assets/logo.jpg";
 
 const SITE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://cra-ssr.herokuapp.com';
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://cra-ssr.herokuapp.com";
 
-const FACEBOOK_APP_ID = 'XXXXXXXXX';
+const FACEBOOK_APP_ID = "XXXXXXXXX";
 
-const defaultTitle = 'My Website';
+const defaultTitle = "My Website";
 const defaultDescription =
-  'This is a really awesome website where we can render on the server. Supa cool.';
+  "This is a really awesome website where we can render on the server. Supa cool.";
 const defaultImage = `${SITE_URL}${logo}`;
-const defaultTwitter = '@cereallarceny';
-const defaultSep = ' | ';
+const defaultTwitter = "@openminedorg";
+const defaultSep = " | ";
 
 class Page extends Component {
   getMetaTags(
@@ -42,40 +42,40 @@ class Page extends Component {
     const theImage = image ? `${SITE_URL}${image}` : defaultImage;
 
     const metaTags = [
-      { itemprop: 'name', content: theTitle },
-      { itemprop: 'description', content: theDescription },
-      { itemprop: 'image', content: theImage },
-      { name: 'description', content: theDescription },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:site', content: defaultTwitter },
-      { name: 'twitter:title', content: theTitle },
-      { name: 'twitter:description', content: theDescription },
-      { name: 'twitter:creator', content: twitter || defaultTwitter },
-      { name: 'twitter:image:src', content: theImage },
-      { property: 'og:title', content: theTitle },
-      { property: 'og:type', content: contentType || 'website' },
-      { property: 'og:url', content: SITE_URL + pathname },
-      { property: 'og:image', content: theImage },
-      { property: 'og:description', content: theDescription },
-      { property: 'og:site_name', content: defaultTitle },
-      { property: 'fb:app_id', content: FACEBOOK_APP_ID }
+      { itemprop: "name", content: theTitle },
+      { itemprop: "description", content: theDescription },
+      { itemprop: "image", content: theImage },
+      { name: "description", content: theDescription },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:site", content: defaultTwitter },
+      { name: "twitter:title", content: theTitle },
+      { name: "twitter:description", content: theDescription },
+      { name: "twitter:creator", content: twitter || defaultTwitter },
+      { name: "twitter:image:src", content: theImage },
+      { property: "og:title", content: theTitle },
+      { property: "og:type", content: contentType || "website" },
+      { property: "og:url", content: SITE_URL + pathname },
+      { property: "og:image", content: theImage },
+      { property: "og:description", content: theDescription },
+      { property: "og:site_name", content: defaultTitle },
+      { property: "fb:app_id", content: FACEBOOK_APP_ID }
     ];
 
     if (noCrawl) {
-      metaTags.push({ name: 'robots', content: 'noindex, nofollow' });
+      metaTags.push({ name: "robots", content: "noindex, nofollow" });
     }
 
     if (published) {
-      metaTags.push({ name: 'article:published_time', content: published });
+      metaTags.push({ name: "article:published_time", content: published });
     }
     if (updated) {
-      metaTags.push({ name: 'article:modified_time', content: updated });
+      metaTags.push({ name: "article:modified_time", content: updated });
     }
     if (category) {
-      metaTags.push({ name: 'article:section', content: category });
+      metaTags.push({ name: "article:section", content: category });
     }
     if (tags) {
-      metaTags.push({ name: 'article:tag', content: tags });
+      metaTags.push({ name: "article:tag", content: tags });
     }
 
     return metaTags;
@@ -88,16 +88,16 @@ class Page extends Component {
       <div id={id} className={className}>
         <Helmet
           htmlAttributes={{
-            lang: 'en',
+            lang: "en",
             itemscope: undefined,
-            itemtype: `http://schema.org/${rest.schema || 'WebPage'}`
+            itemtype: `http://schema.org/${rest.schema || "WebPage"}`
           }}
           title={
             rest.title ? rest.title + defaultSep + defaultTitle : defaultTitle
           }
           link={[
             {
-              rel: 'canonical',
+              rel: "canonical",
               href: SITE_URL + this.props.location.pathname
             }
           ]}
